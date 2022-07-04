b9023921bb994723652003c07bdf3d19b1a3ff51
Dawit Aseged <dawit.aseged@gmail.com>
2022-07-03 23:46:44 -0700

    Decimal Comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch = '0';
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+		if (ch != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		else
+			putchar('\n');
+	}
+
+	return (0);
+}
