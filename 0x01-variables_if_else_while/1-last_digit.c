51c26309e2133ab82ee6725810e7c721d937ec93
Dawit Aseged <dawit.aseged@gmail.com>
2022-07-03 22:43:33 -0700

    Last Digit compiles bug fix

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,9 +20,9 @@ int main(void)
 
 	printf("Last digit of %d is %d ", n, lastDigit);
 
-	if (n == 0)
-		printf("and is 0");
-	else if (n > 5)
+	if (lastDigit== 0)
+		printf("and is 0\n");
+	else if (lastDigit > 5)
 		printf("and is greater than 5\n");
 	else
 		printf("and is less than 6 and not 0\n");
