19c3e6a10523b46840c57e479d2dfea23ccddcd5
Dawit Aseged <dawit.aseged@gmail.com>
2022-07-03 23:23:31 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch = '0';
+
+	for (ch = '0'; ch <= '9'; ch++)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
