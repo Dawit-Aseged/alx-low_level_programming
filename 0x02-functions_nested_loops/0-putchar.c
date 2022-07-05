1d4c14dd8151f33695df32b710ce9b0b4732cb98
Dawit Aseged <dawit.aseged@gmail.com>
2022-07-05 11:21:16 -0700

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	/* your code goes there */
+	return (0);
+}
