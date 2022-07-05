97989c52131e4f624d1bdcc83d0e2d251010dd16
Dawit Aseged <dawit.aseged@gmail.com>
2022-07-05 11:38:13 -0700

    Putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	print_alphabet();
+	/* your code goes there */
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,3 +11,13 @@ void __putchar(void)
 	putchar('r');
 	putchar('\n');
 }
+
+void print_alphabet(void)
+{
+	char ch = 'a';
+	for(ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+}
