10c7e103fdc806b2ec7fcb506a7ba03c645482b8
Dawit Aseged <dawit.aseged@gmail.com>
2022-07-07 01:24:33 -0700

    1 - 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers - prints the numbers
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+				_putchar('1');
+			_putchar(48 + i % 10);
+			if (i == 14)
+				_putchar('\n');
+		}
+	}
+}
