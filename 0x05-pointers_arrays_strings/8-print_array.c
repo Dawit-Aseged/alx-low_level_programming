e0ae2edaadd6d484a31df27be2066b9c2cd699d5
Dawit Aseged <dawit.aseged@gmail.com>
2022-07-12 05:54:26 -0700

    Print Array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - prints array
+ * @a: array
+ * @n: number
+ * Return: none
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		printf("%d, ", a[i]);
+	}
+	printf("%d", a[n-1]);
+	putchar('\n');
+}
