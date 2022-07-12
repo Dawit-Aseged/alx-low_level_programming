01bc9fb15ed9c47d6a158601159337be30b4ab8c
Dawit Aseged <dawit.aseged@gmail.com>
2022-07-12 02:34:55 -0700

    Print Rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *  print_rev - prints the string and adds a new line
+ * @s: char array
+ * Return: none
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	while (i-- >= 0)
+	{
+		putchar(s[i]);
+	}
+	putchar('\n');
+}
